ae1941778a1e8c87d96658021d7e89af73880c41
Zeno Chan <zenochan@ZenodeMac-mini.lan>
2020-04-18 00:51:26 +0800

    修复文件下载未取消订阅bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,9 @@ export class AppComponent
 
   options: PositionInfo = {x: 381, y: 560, w: 437, h: 437};
 
+  history: PositionInfo[] = JSON.parse(localStorage.getItem('history') || '[]');
   preset: PositionInfo[] = [
-    {name: '日历预设', x: 46, y: 1728, w: 170, h: 170},
+    {name: '日历预设', x: 46, y: 1728, w: 172, h: 172},
     {name: '单款预设', x: 381, y: 560, w: 437, h: 437},
   ];
 
@@ -32,12 +33,34 @@ export class AppComponent
   save()
   {
     this.downloadService.downloadEvent.emit(true);
+    this.cacheOptions();
+  }
+
+  cacheOptions()
+  {
+    let index = this.preset.findIndex(item => item.x === this.options.x && item.y === this.options.y && item.w === this.options.w);
+    if (index === -1) {
+      index = this.history.findIndex(item => item.x === this.options.x && item.y === this.options.y && item.w === this.options.w);
+      if (index !== -1) {
+        this.history.splice(index, 1);
+      }
+      this.options.name = new Date().toString();
+      this.history.unshift(JSON.parse(JSON.stringify(this.options)));
+      localStorage.setItem('history', JSON.stringify(this.history));
+    }
+
   }
 
   setOptions(info: PositionInfo)
   {
     this.options = JSON.parse(JSON.stringify(info));
     this.refresh();
+  }
+
+  delete(item: PositionInfo)
+  {
+    this.history = this.history.filter(res => res !== item);
+    localStorage.setItem('history', JSON.stringify(this.history));
   }
 }
 
